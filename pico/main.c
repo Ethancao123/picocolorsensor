c4262dbd2c9cd0bc0a0cb67b19a1b52b801b4f62
Ethancao123 <ethan.cao123456@gmail.com>
2022-03-30 22:42:48 -0700

    pls work

--- pico/main.c
+++ pico/main.c
@@ -5,6 +5,7 @@
 #include "hardware/uart.h"
 #include "hardware/i2c.h"
 #include "pico/binary_info.h"
+#include "hardware/pwm.h"
 
 #define SENSOR_ADDRESS 0x52
 
@@ -29,6 +30,11 @@
 #define PROXIMITY_SENSOR_PULSES 32
 
 #define EXPECTED_PART_ID 0xC2
+
+
+#define RED_THRESH
+#define BLUE_THRESH
+
 
 bool read_i2c_data(i2c_inst_t *i2c, uint8_t reg, uint8_t* buffer, uint8_t readLen) {
     buffer[0] = reg;
@@ -64,9 +70,9 @@ void init_device(i2c_inst_t* i2c, uint8_t* i2cBuffer) {
 
 int main() {
     // Init uart
-    uart_init(uart0, 115200);
-    gpio_set_function(PICO_DEFAULT_UART_TX_PIN, GPIO_FUNC_UART);
-    gpio_set_function(PICO_DEFAULT_UART_RX_PIN, GPIO_FUNC_UART);
+    // uart_init(uart0, 115200);
+    // gpio_set_function(PICO_DEFAULT_UART_TX_PIN, GPIO_FUNC_UART);
+    // gpio_set_function(PICO_DEFAULT_UART_RX_PIN, GPIO_FUNC_UART);
 
     // Init I2c 0
     i2c_init(i2c0, 400000);
@@ -76,44 +82,49 @@ int main() {
     gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
 
     // Init I2c 1
-    i2c_init(i2c1, 400000);
-    gpio_set_function(6, GPIO_FUNC_I2C);
-    gpio_set_function(7, GPIO_FUNC_I2C);
-    gpio_pull_up(6);
-    gpio_pull_up(7);
+    // i2c_init(i2c1, 400000);
+    // gpio_set_function(6, GPIO_FUNC_I2C);
+    // gpio_set_function(7, GPIO_FUNC_I2C);
+    // gpio_pull_up(6);
+    // gpio_pull_up(7);
+
+    //GPIO0 = pin 1; GPIO2 = pin 4
+
 
     // Channel 0 output
-    gpio_init(14);
-    gpio_set_dir(14, GPIO_OUT);
-    gpio_put(14, 1);
+    gpio_init(0);
+    gpio_set_dir(0, GPIO_OUT);
+    gpio_set_function(0, GPIO_FUNC_PWM);
+    redSlice=pwm_gpio_to_slice_num (0); 
+    redChannel=pwm_gpio_to_channel (0);
+    pwm_set_enabled (redSlice, 1);
 
     // Channel 1 output
-    gpio_init(15);
-    gpio_set_dir(15, GPIO_OUT);
-    gpio_put(15, 1);
+    gpio_init(2);
+    gpio_set_dir(2, GPIO_OUT);
+    gpio_set_function(2, GPIO_FUNC_PWM);
+    blueSlice=pwm_gpio_to_slice_num (2); 
+    blueChannel=pwm_gpio_to_channel (2); 
+    pwm_set_enabled (blueSlice, 1);
 
     char outputBuffer[512];
 
     unsigned int values[10];
     uint8_t i2cBuffer[20];
     bool currentValid0 = false;
-    bool currentValid1 = false;
+    // bool currentValid1 = false;
     absolute_time_t loopTime;
     bool reset;
-
-    sleep_ms(100);
 
     init_device(i2c0, i2cBuffer);
     read_i2c_data(i2c0, MAIN_STATUS_REGISTER, i2cBuffer, 1);
 
-    init_device(i2c1, i2cBuffer);
-    read_i2c_data(i2c1, MAIN_STATUS_REGISTER, i2cBuffer, 1);
+    // init_device(i2c1, i2cBuffer);
+    // read_i2c_data(i2c1, MAIN_STATUS_REGISTER, i2cBuffer, 1);
 
     memset(values, 0, sizeof(values));
 
     while (1) {
-        loopTime = make_timeout_time_ms(25);
-
         currentValid0 = read_i2c_data(i2c0, MAIN_STATUS_REGISTER, i2cBuffer, 15);
         if (currentValid0) {
             if ((i2cBuffer[0] & 0x20) != 0) {
@@ -127,24 +138,27 @@ int main() {
             }
         }
 
-        currentValid1 = read_i2c_data(i2c1, MAIN_STATUS_REGISTER, i2cBuffer, 15);
-        if (currentValid1) {
-            if ((i2cBuffer[0] & 0x20) != 0) {
-                init_device(i2c1, i2cBuffer);
-            } else {
-                values[9] = ((i2cBuffer[1] & 0xFF) | ((i2cBuffer[2] & 0xFF) << 8)) & 0x7FF;
-                values[8] = ((i2cBuffer[3] & 0xFF) | ((i2cBuffer[4] & 0xFF) << 8) | ((i2cBuffer[5] & 0xFF) << 16)) & 0x03FFFF;
-                values[7] = ((i2cBuffer[6] & 0xFF) | ((i2cBuffer[7] & 0xFF) << 8) | ((i2cBuffer[8] & 0xFF) << 16)) & 0x03FFFF;
-                values[6] = ((i2cBuffer[9] & 0xFF) | ((i2cBuffer[10] & 0xFF) << 8) | ((i2cBuffer[11] & 0xFF) << 16)) & 0x03FFFF;
-                values[5] = ((i2cBuffer[12] & 0xFF) | ((i2cBuffer[13] & 0xFF) << 8) | ((i2cBuffer[14] & 0xFF) << 16)) & 0x03FFFF;
-            }
-        }
+        // currentValid1 = read_i2c_data(i2c1, MAIN_STATUS_REGISTER, i2cBuffer, 15);
+        // if (currentValid1) {
+        //     if ((i2cBuffer[0] & 0x20) != 0) {
+        //         init_device(i2c1, i2cBuffer);
+        //     } else {
+        //         values[9] = ((i2cBuffer[1] & 0xFF) | ((i2cBuffer[2] & 0xFF) << 8)) & 0x7FF;
+        //         values[8] = ((i2cBuffer[3] & 0xFF) | ((i2cBuffer[4] & 0xFF) << 8) | ((i2cBuffer[5] & 0xFF) << 16)) & 0x03FFFF;
+        //         values[7] = ((i2cBuffer[6] & 0xFF) | ((i2cBuffer[7] & 0xFF) << 8) | ((i2cBuffer[8] & 0xFF) << 16)) & 0x03FFFF;
+        //         values[6] = ((i2cBuffer[9] & 0xFF) | ((i2cBuffer[10] & 0xFF) << 8) | ((i2cBuffer[11] & 0xFF) << 16)) & 0x03FFFF;
+        //         values[5] = ((i2cBuffer[12] & 0xFF) | ((i2cBuffer[13] & 0xFF) << 8) | ((i2cBuffer[14] & 0xFF) << 16)) & 0x03FFFF;
+        //     }
+        // }
 
         snprintf(outputBuffer, sizeof(outputBuffer), "%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u\n",
             currentValid0, currentValid1, values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8], values[9]);
+            //values 0 red??? 
+            //values 2 blue???
+        pwm_set_chan_level (uint redSlice, uint redChannel, uint16_t values[0]);
+        pwm_set_chan_level (uint blueSlice, uint blueChannel, uint16_t values[2]);
+        
 
-        uart_puts(uart0, outputBuffer);
-
-        sleep_until(loopTime);
+        // uart_puts(uart0, outputBuffer);
     }
 }
